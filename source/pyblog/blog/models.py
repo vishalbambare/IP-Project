c3f3e2a555311a7305ab6c65640ec5e9c0c4cb57
pachinko2821 <pachinko28@protonmail.com>
2020-10-25 16:37:13 +0530

    Added image rendering

--- source/pyblog/blog/models.py
+++ source/pyblog/blog/models.py
@@ -11,7 +11,7 @@ STATUS = (
 def get_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = "{}.{}".format(uuid.uuid4(), ext)
-    return os.path.join('static/images/posts/', filename)
+    return os.path.join('images', filename)
 
 class Post(models.Model):
     
