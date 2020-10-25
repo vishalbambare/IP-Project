ad463366761f4b93545e093a9abd14cc5d58aefd
pachinko2821 <pachinko28@protonmail.com>
2020-10-25 16:42:51 +0530

    Added django-summernote

--- source/pyblog/blog/admin.py
+++ source/pyblog/blog/admin.py
@@ -1,11 +1,13 @@
+from django_summernote.admin import SummernoteModelAdmin
 from django.contrib import admin
 from .models import *
 # Register your models here.
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 admin.site.register(Post, PostAdmin)
--- source/pyblog/pyblog/urls.py
+++ source/pyblog/pyblog/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
+    path('', include('blog.urls')),
+    path('summernote', include('django_summernote.urls'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
