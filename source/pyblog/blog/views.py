c006ab00e21906ec56eb37d29f78d832fc543afb
pachinko2821 <pachinko28@protonmail.com>
2020-11-01 09:44:00 +0530

    enabled search feature

--- source/pyblog/blog/views.py
+++ source/pyblog/blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from .models import Post
 from django.views import generic
+
 # Create your views here.
 
-class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+class PostList(generic.ListView): #for creating post cards
+    model = Post
     template_name = 'index.html'
+    context_object_name = "all_search_results" #to render results in template with "for x in all_search_results"
 
-class PostDetail(generic.DetailView):
+    def get_queryset(self):
+        result = super(PostList, self).get_queryset()
+        query = self.request.GET.get('search') #search parameter
+        if query:
+            result = Post.objects.filter(status=1, title__contains=query).order_by('-created_on') #find a way to search from multiple fields
+        else:
+            result = Post.objects.filter(status=1).order_by('-created_on') #return all posts if empty search parameter
+        return result
+
+class PostDetail(generic.DetailView): #for viewing post content
     model = Post
     template_name = 'post_detail.html'
