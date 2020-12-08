3692f86da493f26827aee9041cdf8140e4415a7f
pachinko2821 <pachinko28@protonmail.com>
2020-12-08 16:51:54 +0530

    dk what happend

--- pyblog/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('profile/<slug:slug>/', views.AuthorDetail.as_view(), name='author_detail'),
-    path('team/about/', views.about_us)
-]
